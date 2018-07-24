a2baea5d378fc331530ad88d82cae1f36c93ca7a
Kirill Khudyakov <hudiakov@gmail.com>
2018-07-24 13:53:44 +0300

    Init xcode project

---
+++ FollowTheTeaApp/FollowTheTeaApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,29 @@
+//
+//  InterfaceController.swift
+//  FollowTheTeaApp WatchKit Extension
+//
+//  Created by Kirill Khudiakov on 24.07.2018.
+//  Copyright © 2018 Kirill Khudiakov. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        super.didDeactivate()
+    }
+
+}
---
+++ FollowTheTeaApp/FollowTheTeaApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  FollowTheTeaApp
+//
+//  Created by Kirill Khudiakov on 24.07.2018.
+//  Copyright © 2018 Kirill Khudiakov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
